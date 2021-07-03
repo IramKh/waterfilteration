ed4de0887678f2d4ce3034c8eaa4acf82ae7b02b
IramKh <86776479+IramKh@users.noreply.github.com>
2021-07-03 14:22:09 +0500

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import fields
-from django.forms import ModelForm
-from . models import *
-
-class CustomerUpdateForm(ModelForm):
-    class Meta:
-        model = mukarram
-        fields = '__all__'
-
