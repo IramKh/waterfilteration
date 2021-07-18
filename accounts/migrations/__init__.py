25b283bfeb10d5abe6ba6d2cece36e916842d653
IramKh <86776479+IramKh@users.noreply.github.com>
2021-07-18 07:08:03 +0500

    Add files via upload

---
+++ accounts/migrations/__init__.py
@@ -0,0 +1,16 @@
+#
+#
+#Examples:
+# views
+#   	<li><a href="{% url 'index'%}">Home</a></li>
+
+    #<li><a href="{% url 'products'%}">Products</a></li>
+
+    #<li><a href="{% url 'checkout'%}">Checkout</a></li>
+
+    #<li>
+        #<a href="#" class="dropdown-toggle">About</a>
+
+#
+#
+#"""
